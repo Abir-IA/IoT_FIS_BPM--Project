bb02b49fd9e0094829c69ffdbc35d57255942ff7
Abir-IA <abir.alaoui27@gmail.com>
2019-05-08 15:49:28 +0000

    data extraction from device dataset

--- EventcontrollerFIS.m
+++ EventcontrollerFIS.m
@@ -16,6 +16,9 @@
 % a small xls file with random events
 %[num,txt,tab]=csvread('EventDataSet.csv');
 
+%Data for the first input of the first controller: input about patient
+%status
+
 [num,txt,tab]=xlsread('eventdatasample.xls');
 
 devent=cell2mat({tab{2:end,1}}')
@@ -100,84 +103,39 @@ inScore=S(i).IntervalleScore;
 t=[idevent; source; intervalleSaison; nivChutePrec; dureeChutePrec; scorePatient; freqChutePatient; chuteurRep; idniveauurgence; intervalleSemaine; intervalleChutePrec; intervalleChutePrec; intervalleScore];
 
 
-% another way to access data from data set
-%we will decide later with once to choose for the rest of the code
-%idevent=  dataTest(:, 1);
-%source= dataTest(:, 2);
-%date= dataTest(:, 3);
-%heure= dataTest(:, 4);
-%intervalleJour= dataTest(:, 5);
-%intervalleSaison= dataTest(:, 6);
-%nivChutePrec= dataTest(:, 7);
+%Data for the second input of the first controller: input about device
+%status
+[num,txt,tabb]=csvread('DeviceDataSet.csv');
 
-
+ source=cell2mat({tabb{2:end,1}}');
+ 
+ %ID_Device_Cam=cell2mat({tab{2:end,2}}'); 
+ 
+ID_Device=({tabb{2:end,2}}');
+id=cellfun(@ischar,ID_Device);
+ID_Device(id)={nan}
+ID_Device_Cam=cell2mat(ID_Device); 
+ 
+ dateDevice=({tabb{2:end,3}}');
+ formatOut = 'dd/mm/yyyy';
+ DateStringIn=dateDevice;
+ Date_First_use=datestr(DateStringIn,formatOut);
+ 
+ Total_FalseAlerts_2015=cell2mat({tabb{2:end,4}}');
+ 
+ Total_FalseAlerts_2016=cell2mat({tabb{2:end,5}}');
+ 
+ Total_FalseAlerts_2017=cell2mat({tabb{2:end,6}}');
+ 
+ Total_FalseAlerts_2018=cell2mat({tabb{2:end,7}}');
+ 
+ Device_Changed=cell2mat({tabb{2:end,8}}');
 
  
-% Our first example without the event dataset
-n=6;
-%the controller inputs 
-for i=1:1:n
-idEvent=[321;654;7655;7643;4578;5432];
-source=[17;18; 10;18;65;76];
-date=[20;19;18;17;15;14];
-intervlaJour=[4;4;4;3;3;2];
-intervlaSais=[4;4;4;4;4;4];
-niveauChte=[2;3;4;2;3;2];
-dureeChute=[97;164;97;204;183;122];
-score=[2.46; 2.48;2.4678;2.445;2.3;2.43];
-freq=[7;7;2;7;2;2];
-chuteur=[1;1;1;1;1;1];
-idniveau=[1;1;1;1;1;1];
-intervSemaine=[4;4;4;4;4;4];
 
-%structure 
-S(i).ID=idEvent(i);
-idevent(i)=S(i).ID;
 
-S(i).SR=source(i);
-sr(i)=S(i).SR;
 
-S(i).Date=date(i);
-dte(i)=S(i).Date;
 
-S(i).IntervlaJour=intervlaJour(i);
-inJ(i)=S(i).IntervlaJour;
-
-S(i).IntervlaSais=intervlaSais(i);
-inLs(i)=S(i).IntervlaSais;
-
-S(i).NiveauChte=niveauChte(i);
-nivCh(i)=S(i).NiveauChte;
-
-S(i).DureeChute=dureeChute(i);
-durCht(i)=S(i).DureeChute;
-
-S(i).Score=score(i);
-score(i)=S(i).Score;
-
-S(i).Freq=freq(i);
-frq(i)=S(i).Freq;
-
-S(i).Chuteur=chuteur(i);
-chteur(i)=S(i).Chuteur;
-
-S(i).IDniveau=idniveau(i);
-idnive(i)=S(i).IDniveau;
-
-S(i).IntervSemaine=intervSemaine(i);
-intSem(i)=S(i).IntervSemaine;
-
-% event at time slot t
-a=[idEvent'; source';date';intervlaJour';intervlaSais';niveauChte']
-V1=a(:,1);
-V2=a(:,2);
-V3=a(:,3);
-V4=a(:,4);
-V5=a(:,5);
-V6=a(:,6);
-c=[V1, V2,V3,V4,V5,V6]
-
-end
 
 
 

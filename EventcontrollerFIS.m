1bb21a094d95cf51c272b44230a735d46fcd34ab
Abir-IA <abir.alaoui27@gmail.com>
2019-05-15 15:52:38 +0000

    FIS1 inputs are ready : StatusInput, DeviceInput

--- EventcontrollerFIS.m
+++ EventcontrollerFIS.m
@@ -6,12 +6,12 @@
 %status
 for i=1:1:13
 
-[num,txt,tab]=xlsread('eventdatasample.xls');
+[num,txt,tab]=xlsread('eventdatasample1.xls');
 
 idevent=cell2mat({tab{2:end,1}}');
 format long g
 
-source = categorical({tab{2:end,2}}');
+source = cell2mat({tab{2:end,2}}');
 
 %Source=cellstr(source);
 %source={tab{2:end,2}}';
@@ -39,10 +39,10 @@ date=datestr(DateStringIn,formatOut);
  dureeChutePrec1(c)={nan};
  dureeChutePrec=cell2mat(dureeChutePrec1);
 
-%scorePatient=categorical({tab{2:end,9}}'); 
+scorePatient=cell2mat({tab{2:end,9}}'); 
 %scorePatient = str2double({tab{2:end,9}}');
-scorePatient={tab{2:end,9}}';
-ScorePatient=str2double(scorePatient);
+%scorePatient={tab{2:end,9}}';
+%ScorePatient= typecast(str2double(scorePatient), 'double');
 
 freqChutePatient=cell2mat({tab{2:end,10}}');
 chuteurRep=cell2mat({tab{2:end,11}}');
@@ -53,8 +53,7 @@ intervalleSemaine=cell2mat({tab{2:end,13}}');
 %use Table instead of matrix to solve the problem
 %Table array with named variables that can contain different types
  %create input table : for patient
-EventTable = table(idevent, source, date, heure, intervalleJour, intervalleSemaine, intervalleSaison, nivChutePrec, dureeChutePrec, ScorePatient, freqChutePatient, chuteurRep, idniveau_urgence, dureeChutePrec)
-
+%EventTable = table(idevent, source, date, heure, intervalleJour, intervalleSemaine, intervalleSaison, nivChutePrec, dureeChutePrec, ScorePatient, freqChutePatient, chuteurRep, idniveau_urgence, dureeChutePrec)
 
 
 %Structure for the first dataset
@@ -88,7 +87,7 @@ S(i).DureeChute=dureeChutePrec(i);
 durCht(i)=S(i).DureeChute;
 
 S(i).Score=scorePatient(i);
-score(i)=S(i).Score;
+score(i)= S(i).Score;
 
 S(i).Freq=freqChutePatient(i);
 frq(i)=S(i).Freq;
@@ -105,20 +104,25 @@ idniveau(i)=S(i).IDniveau_urgence;
 S(i).IntervalleSemaine=intervalleSemaine(i);
 intervSemaine(i)=S(i).IntervalleSemaine;
 
-k=[num2str(idev'),strvcat(cellstr(sour)),strvcat(cellstr(dte)),strvcat(cellstr(heuree)),num2str(inJ'),num2str(inLs'),num2str(nivCh'),num2str(durCht'),strvcat(cellstr(score)),num2str(frq'),num2str(chteur'),num2str(chte'),num2str(idniveau'),num2str(intervSemaine')]
-str1=k(1,:);
-str2=k(2,:);
-str3=k(3,:);
-str4=k(4,:);
-str5=k(5,:);
-str6=k(6,:);
-str7=k(7,:);
-str8=k(8,:);
-str9=k(9,:);
-str10=k(10,:);
-str11=k(11,:);
-str12=k(12,:);
-str13=k(13,:);
+%k=[num2str(idev'),strvcat(cellstr(sour)),strvcat(cellstr(dte)),strvcat(cellstr(heuree)),num2str(inJ'),num2str(inLs'),num2str(nivCh'),num2str(durCht'),strvcat(cellstr(score)),num2str(frq'),num2str(chteur'),num2str(chte'),num2str(idniveau'),num2str(intervSemaine')]
+
+ StatusInput=[idev', sour', inJ', inLs', nivCh', durCht', score', frq', chteur', chte', idniveau', intervSemaine']
+% 
+% 
+% 
+% str1=k(1,:);
+% str2=k(2,:);
+% str3=k(3,:);
+% str4=k(4,:);
+% str5=k(5,:);
+% str6=k(6,:);
+% str7=k(7,:);
+% str8=k(8,:);
+% str9=k(9,:);
+% str10=k(10,:);
+% str11=k(11,:);
+% str12=k(12,:);
+% str13=k(13,:);
 
 
 %Data for the second input of the first controller: input about device
@@ -127,14 +131,17 @@ str13=k(13,:);
  
  
  %Position=cell2mat({raw{2:end,2}}');
- Position = categorical({tab{2:end,2}}');
+ Position = cell2mat({tab{2:end,2}}');
  
  ID_Device_Cam=cell2mat({tab{2:end,1}}'); 
  
- dateDevice=({tab{2:end,3}}');
- formatOut = 'dd/mm/yyyy';
- DateStringIn=dateDevice;
- Date_First_use=datestr(DateStringIn,formatOut);
+%  dateDevice=({tab{2:end,3}}');
+%  formatOut = 'dd/mm/yyyy';
+%  DateStringIn=dateDevice;
+%  Date_First_use=datestr(DateStringIn,formatOut);
+%  
+%  DateFU = datenum({tab{2:end,3}}');
+%  
  
  Total_FalseAlerts_2015=cell2mat({tab{2:end,4}}');
  
@@ -148,7 +155,7 @@ str13=k(13,:);
  
  %create input table : for device
  
-InputDeviceTable = table(Position, ID_Device_Cam, Date_First_use, Total_FalseAlerts_2015, Total_FalseAlerts_2016, Total_FalseAlerts_2017, Total_FalseAlerts_2018, Device_Changed);
+InputDeviceTable = table(Position, ID_Device_Cam, Total_FalseAlerts_2015, Total_FalseAlerts_2016, Total_FalseAlerts_2017, Total_FalseAlerts_2018, Device_Changed);
  
 
 %Structure for the second dataset
@@ -159,8 +166,8 @@ ID_Dev(i)=A(i).IDDevice;
 A(i).Posit= Position(i);
 pos(i)=A(i).Posit;
 
-A(i).dteDev=dateDevice(i);
-dtDeivce(i)=A(i).dteDev;
+% A(i).dteDev=dateDevice(i);
+% dtDeivce(i)=A(i).dteDev;
 
 A(i).tFalseAlerts15=Total_FalseAlerts_2015(i);
 Total_FalseAlerts15(i)=A(i).tFalseAlerts15;
@@ -180,9 +187,10 @@ A(i).Dev_Changed=Device_Changed(i);
 DevChanged(i)=A(i).Dev_Changed;
 
 
-k2=strvcat([num2str(ID_Dev(i));pos(i);dtDeivce(i);num2str(Total_FalseAlerts15(i));num2str(Total_FalseAlerts16(i));num2str(Total_FalseAlerts17(i));num2str(Total_FalseAlerts18(i));num2str(DevChanged(i))]);
+%k2=strvcat([num2str(ID_Dev(i)); pos(i); num2str(Total_FalseAlerts15(i)); num2str(Total_FalseAlerts16(i)); num2str(Total_FalseAlerts17(i)); num2str(Total_FalseAlerts18(i)); num2str(DevChanged(i))]);
 
 
+DeviceInput=[ID_Dev', pos', Total_FalseAlerts15', Total_FalseAlerts16', Total_FalseAlerts17', Total_FalseAlerts18', DevChanged']
 
 
 
